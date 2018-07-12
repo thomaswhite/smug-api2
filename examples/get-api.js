f2be87a57e4f3e3666715fcc13a356a6fafdcf0f
Thomas <thomas.0007@gmail.com>
2018-07-12 23:09:03 +0100

    get-api work in progress

--- examples/get-api.js
+++ examples/get-api.js
@@ -1,8 +1,12 @@
 const creds = require('../lib/credentials.js')()
 const smug = require('../index.js')(creds)
-
+const get_api_url = require('../lib/get-api.js')(creds)
 const util = require('util')
 
+const url = get_api_url.getUrl('User')
+console.info(url)
+
+/*
 smug.connect() // getAPI
   .then(function (data) {
     console.log(util.inspect(data, {showHidden: false, depth: null}))
@@ -10,3 +14,4 @@ smug.connect() // getAPI
   .catch(function (err) {
     console.log(err)
   })
+*/
